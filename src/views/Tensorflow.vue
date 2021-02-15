318aecc1722c7bff3c77319b33204e4b7fe02cc6
leandrocodes <leandrovianacodes@gmail.com>
2021-02-15 17:49:10 -0400

    tensorflowjs with http-server

---
+++ src/views/Tensorflow.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Tensorflow</h1>
+</template>
+
+<script>
+import * as tf from '@tensorflow/tfjs'
+export default {
+  data: () => ({
+    model: null
+  }),
+  async created() {
+    this.model = await tf.loadLayersModel(
+      'http://127.0.0.1:9090/src/keras/model.json'
+    )
+  }
+}
+</script>
